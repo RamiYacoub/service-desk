8a207e1d4d0e3601c2be3fa9e49acecc86573ea9
Rami <ramiaref61@gmail.com>
2026-01-01 17:50:10 +0200

    Config: stop tracking application.properties and add example config

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.user.UserEntity;
-import com.example.demo.user.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +11,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
